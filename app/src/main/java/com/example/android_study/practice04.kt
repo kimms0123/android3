70365641e65cc5ce9e8b1c9d029c5c3dbbd12a5a
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2025-05-12 23:42:48 +0900

    day05

---
+++ app/src/main/java/com/example/android_study/practice04.kt
@@ -0,0 +1,10 @@
+package com.example.android_study
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class practice04 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
